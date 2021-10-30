a6e5fb2532fc5693ac9ef66474f1092d0c6348df
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-31 00:53:49 +0300

    set up registration

--- myauth/serializers.py
+++ myauth/serializers.py
@@ -2,17 +2,18 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-  passwords = serializers.CharField(max_length=200,min_length=6,write_only=True)
+  password= serializers.CharField(max_length=200,min_length=6,write_only=True)
   email= serializers.EmailField(max_length=200,min_length=6)
   first_name = serializers.CharField(max_length=200,min_length=6)
   last_name = serializers.CharField(max_length=200,min_length=6)
 
   class Meta:
     model = User
-    fields=['username', 'first_name','last_name', 'email']
+    fields=['username', 'first_name','last_name', 'email','password']
 
   def validate(self,attrs):
-    if User.objects.filter(email=attrs['email']).exists():
+    email= attrs.get('email', '')
+    if User.objects.filter(email=email).exists():
       raise serializers.ValidationError({'email',('Email is already taken SORRYY!!')})  
 
     return super().validate(attrs) 

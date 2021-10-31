2460e4c7a03b681821997053d6698a0ab31d2513
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-31 13:03:20 +0300

    add log in site

--- myauth/backends.py
+++ myauth/backends.py
@@ -5,4 +5,10 @@ from rest_framework import authentication
 class JwtAuthentication(authentication.BaseAuthentication):
 
   def authenticate(self,request):
+    auth_data=authentication.get_authorization_header(request)
+
+    if not auth_data:
+      return None
+
+
     return super().authenticate(request)

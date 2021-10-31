bf2fc4bc0546b5057f756ec6cfdc8d4f15de87d7
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-31 15:35:02 +0300

    add models

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Flashcard(models.Model):
   name=models.ForeignKey(to=User,on_delete=models.CASCADE)
   subject=models.CharField(max_length=200)
   body=models.CharField(max_length=200)
+  is_complete=models.BooleanField(default=False)
